e38b1ade2536a3c198554df9c7e080c3dbd28bc6
“[bhanumalhotra92]” <[bhanu.malhotra@karglobal.com]>
2023-10-03 17:22:15 +0530

    updateWithmoreconcepts

--- projects/src/backend_for_a_user_interface/main.go
+++ projects/src/backend_for_a_user_interface/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
+
+	var err error = http.ListenAndServe("localhost:4242", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint was called")
 }
